0b542fa63f6ed3c9e51d3ef36f6e9fd14554e48c
CNC Padawan <praetorian10@gmail.com>
2018-02-04 02:08:44 -0600

    Updated paths

--- projects/Cura.cmake
+++ projects/Cura.cmake
@@ -1,8 +1,8 @@
 option(CURA_ENABLE_DEBUGMODE "Enable crash handler and other debug options in Cura" OFF)
 
 ExternalProject_Add(Cura
-    GIT_REPOSITORY https://github.com/cncpadawan/Cura
-    GIT_TAG origin/${TAG_OR_BRANCH}
+    GIT_REPOSITORY https://github.com/peopoly3d/Cura
+    GIT_TAG origin/Moai${TAG_OR_BRANCH}
     CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${EXTERNALPROJECT_INSTALL_PREFIX} -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH} -DURANIUM_SCRIPTS_DIR= -DCURA_VERSION=${CURA_VERSION} -DCURA_DEBUGMODE=${CURA_ENABLE_DEBUGMODE}
 )
 
